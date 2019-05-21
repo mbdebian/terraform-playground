27c84994ab1992562f2d9c915ed644769ea3736d
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-21 23:50:39 +0100

    bucket configuration to hold terraform state

--- aws/s3_backbone/s3.tf
+++ aws/s3_backbone/s3.tf
@@ -7,4 +7,26 @@ resource "aws_s3_bucket" "mbdebiantfsection2main" {
   lifecycle {
       prevent_destroy = "true"
   }
+
+  server_side_encryption_configuration {
+      rule {
+          apply_server_side_encryption_by_default {
+              sse_algorithm = "AES256"
+          }
+      }
+  }
+
+  versioning {
+      enabled = true
+  }
+
+  lifecycle_rule {
+      id = "state"
+      prefix = "state/"
+      enabled = true
+
+      noncurrent_version_expiration {
+          days = 90
+      }
+  }
 }
