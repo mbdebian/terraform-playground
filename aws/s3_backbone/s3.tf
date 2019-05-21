23618f31018782c0cb62793d0d57cce2e47cee91
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-21 22:45:44 +0100

    use variables to define the bucket

--- aws/s3_backbone/s3.tf
+++ aws/s3_backbone/s3.tf
@@ -1,4 +1,10 @@
 resource "aws_s3_bucket" "mbdebiantfsection2main" {
-    bucket = "mbdebiantfsection2main"
+    bucket = "${var.s3_bucket_name}"
     acl = "private"
+
+    tags = {
+        created_by = "${lookup(var.s3_tags, "created_by")}"
+    }
+
+    region = "${var.s3_regions[0]}"
 }
