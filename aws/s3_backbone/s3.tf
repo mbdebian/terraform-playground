fb7853ee806602ef847440ead42c63dabfb45d80
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-21 23:18:13 +0100

    deleting a resource by removing it from our infrastructure definition

--- aws/s3_backbone/s3.tf
+++ aws/s3_backbone/s3.tf
@@ -1,8 +1,11 @@
-resource "aws_s3_bucket" "mbdebiantfsection2main" {
+/* resource "aws_s3_bucket" "mbdebiantfsection2main" {
   bucket = "${var.s3_bucket_prefix}-${var.environment}-${var.s3_region}"
   acl    = "private"
-
   tags = "${local.s3_tags}"
-
   region = "${var.s3_region}"
+
+  lifecycle {
+      prevent_destroy = "true"
+  }
 }
+ */
