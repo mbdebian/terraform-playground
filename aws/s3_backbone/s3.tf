224eba34b6fca3ccf8f034000e96f2a6a00e6447
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-21 15:26:18 +0100

    bucket name changed

--- aws/s3_backbone/s3.tf
+++ aws/s3_backbone/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "mbdebiantfmain" {
-    bucket = "packt-terraform-section2-bucket"
+resource "aws_s3_bucket" "mbdebiantfsection2main" {
+    bucket = "mbdebiantfsection2main"
     acl = "private"
 }
