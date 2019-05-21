80bae5cfffbc4219ff98a4d3145a8c72f3514743
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-21 22:59:52 +0100

    refactoring variables

--- aws/s3_backbone/s3.tf
+++ aws/s3_backbone/s3.tf
@@ -2,9 +2,7 @@ resource "aws_s3_bucket" "mbdebiantfsection2main" {
     bucket = "${var.s3_bucket_name}"
     acl = "private"
 
-    tags = {
-        created_by = "${lookup(var.s3_tags, "created_by")}"
-    }
+    tags = "${local.s3_tags}"
 
     region = "${var.s3_regions[0]}"
 }
--- aws/s3_backbone/variables.tf
+++ aws/s3_backbone/variables.tf
@@ -1,19 +1,24 @@
+variable "environment" {
+    type = "string"
+    default = "test"
+}
+
+
 variable "s3_bucket_name" {
     default = "mbdebiantfsection2main"
     description = "Name of the S3 bucket for backbone"
     type = "string"
 }
 
-variable "s3_tags" {
-    type = "map"
-
-    default = {
-        created_by = "Manuel Bernal Llinares"
-        email = "mbdebian@gmail.com"
-    }
-}
-
 variable "s3_regions" {
     type = "list"
-    default = ["us-east-1"]
+    default = ["us-east-1", "us-east-2"]
+}
+
+locals {
+  s3_tags = {
+        created_by = "Manuel Bernal Llinares"
+        email = "mbdebian@gmail.com"
+        environment = "${var.environment}"
+    }
 }
