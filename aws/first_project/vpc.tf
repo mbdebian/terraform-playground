139a5dedfe72ec0162505c064e56029db5ef7d2e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-23 00:32:41 +0100

    platform public gateway definition

--- aws/first_project/vpc.tf
+++ aws/first_project/vpc.tf
@@ -6,4 +6,12 @@ resource "aws_vpc" "project_vpc" {
   }
 }
 
-// 
+// Enable access to / from internet for the instances in the public subnets
+resource "aws_internet_gateway" "platform_public_gateway" {
+  vpc_id = "${aws_vpc.project_vpc.id}"
+
+  tags {
+    "name" = "Platform Public Gateway"
+  }
+}
+
