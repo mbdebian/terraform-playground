78dcd6171d890a7f522a42e279203f6157c40954
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-23 23:03:11 +0100

    open all outbound traffic

--- aws/first_project/bastion.tf
+++ aws/first_project/bastion.tf
@@ -60,3 +60,12 @@ resource "aws_security_group_rule" "bastion_allow_all_ssh" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.bastion.id}"
 }
+
+resource "aws_security_group_rule" "bastion_allow_all_outbound" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.bastion.id}"
+}
