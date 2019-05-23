457152ed2bb9488e4c1e80958966a5dd796a9803
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-23 11:04:29 +0100

    definition for public subnets

--- aws/first_project/subnets.tf
+++ aws/first_project/subnets.tf
@@ -1,6 +1,16 @@
 // Public subnets, each subnet in a different availability zone
 // TODO
+resource "aws_subnet" "subnet_public" {
+  count = "${length(var.platform_availability_zones)}"
+  vpc_id = "${aws_vpc.project_vpc.id}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, count.index)}"
+  availability_zone = "${element(var.platform_availability_zones, count.index)}"
+  map_public_ip_on_launch = true
 
+  tags = {
+      "name" = "Public subnet - ${element(var.platform_availability_zones, count.index)}"
+  }
+}
 
 // Private subnets, each subnet in a different availability zone
 // TODO
