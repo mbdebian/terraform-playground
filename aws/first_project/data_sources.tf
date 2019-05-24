85daaaf66960c0b60e0ed576185c701f9f17ff9b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-24 12:34:09 +0100

    aws instance definition

--- aws/first_project/data_sources.tf
+++ aws/first_project/data_sources.tf
@@ -4,3 +4,21 @@ data "template_file" "bastion_public_key" {
 }
 
 // AWS
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+      name = "name"
+      values = ["amzn-ami-*-x86_64-gp2"]
+  }
+
+  filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+  }
+
+  filter {
+      name = "owner-alias"
+      values = ["amazon"]
+  }
+}
