d77aa70f947175e43af83556e7ae1575b143280a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-24 16:50:04 +0100

    desired capacity changed for test

--- aws/first_project/test_instance.tf
+++ aws/first_project/test_instance.tf
@@ -22,7 +22,7 @@ resource "aws_launch_configuration" "test" {
 resource "aws_autoscaling_group" "test" {
   name                 = "${aws_launch_configuration.test.name}-asg"
   min_size             = 0
-  desired_capacity     = 0
+  desired_capacity     = 1
   max_size             = 1
   health_check_type    = "EC2"
   launch_configuration = "${aws_launch_configuration.test.name}"
