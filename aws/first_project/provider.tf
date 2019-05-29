afd62e4f41bb40d7009ce1cec3cb4edeb4cd9c99
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-29 12:09:56 +0100

    deactivate S3 tfstate bucket

--- aws/first_project/provider.tf
+++ aws/first_project/provider.tf
@@ -11,11 +11,11 @@ provider "template" {
 terraform {
   required_version = ">= 0.11.14"
 
-  backend "s3" {
+  /* backend "s3" {
     # The name of the bucket here is not the ID of that bucket in the terraform files, but the real / final bucket name on S3
     bucket  = "mbdebiandevops-myfirstproject-us-east-1"
     key     = "devops/terraform"
     region  = "us-east-1"
     encrypt = "true"
-  }
+  } */
 }
